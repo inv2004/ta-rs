fbbea77f2cea679691a340435dcf4c53992ffb33
inv2004 <gPoiuhjkl1>
2018-09-28 22:03:47 -0400

    +rma

--- src/indicators/mod.rs
+++ src/indicators/mod.rs
@@ -30,3 +30,6 @@ pub use self::moving_average_convergence_divergence::MovingAverageConvergenceDiv
 
 mod efficiency_ratio;
 pub use self::efficiency_ratio::EfficiencyRatio;
+
+mod relative_moving_average;
+pub use self::relative_moving_average::RelativeMovingAverage;
